03abf74813e03c96c8c094384a2da5380af84551
rita01-F <63978286+rita01-F@users.noreply.github.com>
2020-05-30 13:51:40 +1000

    First

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+include 'menu.php'; //[s]
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Крестики-нолики</title>
+	<link rel="stylesheet" href="style.css">
+	
+</head>
+<body>
+	<div class="field">
+	<div  id="area">
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		<div class="block"></div>
+		
+	</div>
+
+	<div align="center">
+		<button>Начать заново</button>
+	</div>
+	</div>
+	<script type="text/javascript" src="js.js"></script>
+</body>
+</html>
