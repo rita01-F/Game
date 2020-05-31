9cec1df194e78fd7592e759741ba911ee5792a7e
Ritos <kutolvas@bk.ru>
2020-05-31 23:07:11 +1000

    главная

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ include 'menu.php'; //[s]
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<div class="text">
+		<p style="font-size: 30pt;">Добро пожаловать в игру крестики - нолики</p>
+	    </br>
+		<p style="font-size: 22pt;">Выберите тип игры!</p>
+	</div>
 	<script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
 </body>
 </html>
